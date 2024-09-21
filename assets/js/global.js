0b6fc918de7e3ee2ef0c5597c53aa4d4407baf0e
Felix Lehmann <felix_lehmann@ymail.com>
2024-09-21 22:43:11 +0000

    feat: create dashboard.html

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,23 +1,21 @@
-var user_id;
-var user_firstname;
-var user_lastname;
-var username;
-var pw;
+var global_user_id;
+var global_user_firstname;
+var global_user_lastname;
 
-function login(username, pw) {
+
+function login(login_username, login_pw) {
     fetch("./assets/data/data_login.csv").then(response => response.text()).then(data => {
         let lines = data.split("\n");
         let line;
-
-        console.log(username + " " + pw);
         
         for (let i = 0; i < lines.length; i++) {
             line = lines[i].split(";");
             if(line[3] == username && line[4] == pw) {
-                user_id         = line[0];
-                user_firstname  = line[1];
-                user_lastname   = line[2];
-                alert(user_id + "\n" + user_firstname + "\n" + user_lastname + "\n" + username + "\n" + pw);
+                global_user_id         = line[0];
+                global_user_firstname  = line[1];
+                global_user_lastname   = line[2];
+                alert(global_user_id + "\n" + global_user_firstname + "\n" + global_user_lastname);
+                window.location.assign("dashboard.html");
                 return;
             }
         }
