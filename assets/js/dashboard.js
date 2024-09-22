62923cd461554eb2a48309da96fee1013c3e43c1
Felix Lehmann <felix_lehmann@ymail.com>
2024-09-22 02:00:58 +0000

    fix: pass variables through localstorage, not exports
    only user id, account numbers and newly requested
    transactions will be saved temporarily in the storage
    as well as first_name and last_name of the dummy data

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,5 +1,5 @@
-import {global_user} from "./index.js"; 
+document.addEventListener("DOMContentLoaded", function () {
+    //alert(global_user.firstname + " " + global_user.lastname + ": " + global_user.id);
 
-document.addEventListener("DOMContentLoaded", function(){
-    alert(global_user.firstname + " " + global_user.lastname + ": " + global_user.id);
+    alert(localStorage.getItem("firstname") + " " + localStorage.getItem("lastname") + ": " + localStorage.getItem("id"));
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,39 +1,28 @@
-export var global_user = {
-    id:        "",
-    firstname: "",
-    lastname:  "fff"
-}
-
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let login_form = document.getElementById("login_form");
-    
-    login_form.addEventListener("submit", function(event){
+
+    login_form.addEventListener("submit", function (event) {
         event.preventDefault();
-        
-        let username= document.getElementById("username").value;
-        let pw      = document.getElementById("pw").value;
+
+        let username = document.getElementById("username").value;
+        let pw = document.getElementById("pw").value;
         login(username, pw);
-        
     })
-
-
 })
 
 function login(login_username, login_pw) {
     fetch("./assets/data/data_login.csv").then(response => response.text()).then(data => {
         let lines = data.split("\n");
         let line;
-        
+
         for (let i = 0; i < lines.length; i++) {
             line = lines[i].split(";");
-            if(line[3] == login_username && line[4] == login_pw) {
-                global_user = new Object;
-                global_user.id         = line[0];
-                global_user.firstname  = line[1];
-                global_user.lastname   = line[2];
-                
+            if (line[3] == login_username && line[4] == login_pw) {
+                localStorage.setItem("id", line[0]);
+                localStorage.setItem("firstname", line[1]);
+                localStorage.setItem("lastname", line[2]);
 
-                alert(`Welcome back to Wagner Bank, ${global_user.firstname} ${global_user.lastname}! \n` + global_user.id );
+                alert(`Welcome back to Wagner Bank, ${localStorage.getItem("first_name")} ${localStorage.getItem("last_name")}! \n` + localStorage.getItem("id"));
                 window.location.assign("dashboard.html");
                 return;
             }
@@ -44,9 +33,3 @@ function login(login_username, login_pw) {
     return;
 }
 
-export function get_user_id() {
-    let x = global_user_firstname;
-    alert(x);
-    return x;
-
-}
