5844abd388b6f8de0f4b5e022af7a248fa9e48be
Felix Lehmann <felix_lehmann@ymail.com>
2024-09-22 20:09:09 +0000

    feat: structure dashboard html

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -3,6 +3,13 @@ const user_id = "id", user_first = "user_firstname", user_last = "user_lastname"
 document.addEventListener("DOMContentLoaded", function () {
     //alert(global_user.firstname + " " + global_user.lastname + ": " + global_user.id);
 
-    alert(sessionStorage.getItem(user_first) + " " + sessionStorage.getItem(user_last) + ": " + sessionStorage.getItem(user_id));
+    addNameToNavBar();
+
     
 })
+
+function addNameToNavBar() {
+    let span = document.getElementById("nav_bar_right_text");
+    span.innerHTML = `Hello, ${sessionStorage.getItem(user_first)} ${sessionStorage.getItem(user_last)}!`;
+    
+}
