a2883b9354e047be990f65b3eb51baaa6f570750
Felix Lehmann <felix_lehmann@ymail.com>
2024-09-23 19:28:22 +0000

    feat: add functions to fill dashboard_html with content

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -36,20 +36,43 @@ function fillWebsite() {
 }
 
 function addRow(listContainer, line){
+    let fontawesome = document.createElement("i");
+    
+    switch(line[3]) {
+        case "current account":
+            fontawesome.className = "fa-solid fa-building-columns"
+            
+        case "savings account":
+            fontawesome.className = "fa-solid fa-piggy-bank fa-xl"
+        case "credit card":
+            fontawesome.className = "fa-solid fa-building-columns"
+        default:
+    }
+    alert(line[3] + ": " + fontawesome.outerHTML);
+
     listContainer.innerHTML += `
     <div class="listrow background_lblue">
-        <div class="listitem_left textcolor_white">
-            picture
-        </div>
+        <div class="listitem_left textcolor_white"> ${fontawesome.outerHTML} </div>
         <div class="listitem_middle">
             <div class="listitem_middle_top textcolor_white font_cinzel">${line[4]}</div>
             <div class="listitem_middle_bottom textcolor_white ">${line[2]}</div>
         </div>
         <div class="listitem_right">
-            <span class="balance textcolor_white">${line[5]}</span>
+            <span class="balance textcolor_white">${new Intl.NumberFormat('no').format(line[5])}</span>
             <span class=" textcolor_white">EUR</span>
         </div>
     </div>
     `;
 }
 
+function updateBalance(){
+    let balances = document.getElementsByClassName("balance");
+    let total = document.getElementById("db_subtotal_amount")
+    let total_amount = 0;
+    let amount = 0;
+    for(let balance of balances){
+        amount = parseFloat(balance.innerHTML.replace("&nbsp;", "").replace(",",".").replace("−", "-"))
+        total_amount += amount;
+    }
+    total.innerHTML = new Intl.NumberFormat('no').format(total_amount);
+}
