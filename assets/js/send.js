40f0e9e947f9435c40b70b663ea91b0e7a2acdb3
Felix Lehmann <felix_lehmann@ymail.com>
2024-09-26 01:17:20 +0000

    fix: switch receiver container 1 vs 2 for send/move action in send_js

--- assets/js/send.js
+++ assets/js/send.js
@@ -3,14 +3,14 @@ const acc_id = "account_id", acc_kind = "account_kind";
 const action="action";
 
 document.addEventListener("DOMContentLoaded", function () {
-    let move_container = document.getElementById("send_receiver2_container");
-    let send_container = document.getElementById("send_receiver_container");
-
-    if(sessionStorage.getItem(acc_kind)=="move") {
-        move_container.style.display = "block";
+    let move_container = document.getElementById("send_receiver_container");
+    let send_container = document.getElementById("send_receiver2_container");
+    
+    if(sessionStorage.getItem(action)=="move") {
+        move_container.style.display = "flex";
         send_container.style.display = "none";
     } else {
         move_container.style.display = "none";
-        send_container.style.display = "block";
+        send_container.style.display = "flex";
     }
 })
